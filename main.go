ba4c86b6b6a240828e8b23c45c50837305b0359b
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-11-21 17:02:48 -0500

    webserver, HTMX, tailwind

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
-	"github.com/JakeNorman007/horse_lazer/cmd/controllers"
+	// "github.com/JakeNorman007/horse_lazer/cmd/controllers"
 	"github.com/JakeNorman007/horse_lazer/cmd/initializers"
 )
 
@@ -13,12 +14,23 @@ func init(){
 
 func main() {
     r := gin.Default()
+    
+    r.LoadHTMLGlob("src/*")
 
-    r.GET("/", controllers.FriendsShow)
-    r.GET("/:id", controllers.FriendShowById)
-    r.POST("/", controllers.FriendCreate)
-    r.PUT("/:id", controllers.FriendUpdate)
-    r.DELETE("/:id", controllers.FriendDelete)
+    r.GET("/", func(c *gin.Context){
+        c.HTML(http.StatusOK, "index.html", nil)
+    })
+    r.Run(":42069")
 
-    r.Run() //localhost:42069
+    // going to use static data for this project to keep it extra simple
+    // so I can just mess with some HTMX for fun
+    // creating database was more for prcatice for an upcoming project
+    //
+    // r.GET("/", controllers.FriendsShow)
+    // r.GET("/:id", controllers.FriendShowById)
+    // r.POST("/", controllers.FriendCreate)
+    // r.PUT("/:id", controllers.FriendUpdate)
+    // r.DELETE("/:id", controllers.FriendDelete)
+
+    // r.Run() //localhost:42069
 }
