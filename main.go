6d659afc085961fbf0b66b3ff79fbc28407f73bc
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-11-22 14:12:24 -0500

    just needs styling

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 //initializers.ConnectToDB()
 //}
 
-type Friend struct{
+type Contact struct{
     Name    string
     Email   string
     Phone   string
@@ -27,19 +27,30 @@ func main() {
 
     handlerOne := func(w http.ResponseWriter, r *http.Request){
         tmpl := template.Must(template.ParseFiles("src/views/index.html"))
-        friends := map[string][]Friend{
-            "friends": {
+        contacts := map[string][]Contact{
+            "Contacts": {
                 {Name: "Jake Norman", Email: "jake@example.com", Phone: "555-555-5555"},
                 {Name: "Kazmir Dinse", Email: "kazmir@example.com", Phone: "444-444-4444"},
                 {Name: "Elliott Norman", Email: "elliott@example.com", Phone: "333-333-3333"},
             },
         }
-        tmpl.Execute(w, friends)
+        tmpl.Execute(w, contacts)
+    }
+
+    handlerTwo := func(w http.ResponseWriter, r *http.Request){
+        name := r.PostFormValue("name")
+        email := r.PostFormValue("email")
+        phone := r.PostFormValue("phone")
+
+        tmpl := template.Must(template.ParseFiles("src/views/index.html"))
+        tmpl.ExecuteTemplate(w, "contact-list-element", Contact{Name: name, Email: email, Phone: phone})
     }
 
     http.HandleFunc("/", handlerOne)
+    http.HandleFunc("/add-contact/", handlerTwo)
 
     log.Fatal(http.ListenAndServe(":42069", nil))
+
     //r := gin.Default()
 
     // routes, will probably put these in their own file
